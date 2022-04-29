e8861e6a087e10e05d623e414ba5e49cca49ee3f
kirstytheproud <k.joproud@gmail.com>
2022-04-29 12:26:36 +0100

    added cross origin

--- margaritas-api-backend/src/main/java/com/example/margaritasapibackend/MargaritaController.java
+++ margaritas-api-backend/src/main/java/com/example/margaritasapibackend/MargaritaController.java
@@ -1,6 +1,7 @@
 package com.example.margaritasapibackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
